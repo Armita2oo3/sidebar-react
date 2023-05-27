24d6905fc3459a58cccd8ad2739325077d9a56af
Armita2oo3 <mir.armita17@gmail.com>
2023-05-27 15:17:27 +0430

    made the sidebar

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "./conpacts/Sidebar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Homepage from "./pages/Homepage";
+import Profile from "./pages/Profile";
+
+export default function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/homepage" element={<Homepage />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </Sidebar>
+      </BrowserRouter>
+    </div>
+  );
+}
---
+++ src/conpacts/Sidebar.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { AiFillContacts, AiFillHome } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import { NavLink } from "react-router-dom";
+
+export default function Sidebar({ children }) {
+  const [isOpen, setisOpen] = useState(false);
+  function toggle() {
+    setisOpen(!isOpen);
+  }
+  const MenuItem = [
+    {
+      path: "/homepage",
+      name: "HomePage",
+      icon: <AiFillHome />,
+    },
+    {
+      path: "/about",
+      name: "About",
+      icon: <BsFillInfoCircleFill />,
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      icon: <AiFillContacts />,
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      icon: <CgProfile />,
+    },
+  ];
+  return (
+    <div className="container">
+      <div style={{ width: isOpen ? "300px" : "50px" }} className="sidebar">
+        <div className="top">
+          <h1 style={{ display: isOpen ? "block" : "none" }} className="logo">
+            Menu
+          </h1>
+          <div style={{ marginLeft: isOpen ? "80px" : "0px" }} className="bars">
+            <FaBars onClick={toggle} />
+          </div>
+        </div>
+        {MenuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link"
+            activeclassName="active"
+          >
+            <div className="icon">{item.icon}</div>
+            <div
+              style={{ display: isOpen ? "block" : "none" }}
+              className="link-text"
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
+      </div>
+      <main>{children}</main>
+    </div>
+  );
+}
